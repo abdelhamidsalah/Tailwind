e06e7e0c9c9e6d24b8a4ca86628b14be66be5bcc
abdelhamidsalah <abdelhamidsal@gmail.com>
2023-10-21 14:57:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import Footer from './component/Footer';
-import Header from './component/Header';
+import { BrowserRouter , Route , Routes } from 'react-router-dom';
 import './index.css';
-import Home from './pages/Home';
+import Home from './Pages/Home';
+import Shop from './Pages/Shop';
+import Cart from './Pages/Cart';
+import Checkout from './Pages/Checkout';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ProDetails from './components/ProDetails';
+
+
 function App() {
   return (
-    <div className="App">
-     <Header/>
-     <Home/>
-     <Footer/>
-    </div>
+<div>
+<BrowserRouter>
+<Header/>
+<Routes>
+<Route path='/' element={<Home/>}></Route>
+<Route path='/shop' element={<Shop/>}></Route>
+<Route path='/cart' element={<Cart/>}></Route>
+<Route path='/checkout' element={<Checkout/>}></Route>
+<Route path='/login' element={<Login/>}></Route>
+<Route path='/signup' element={<Signup/>}></Route>
+<Route path='/shop/:id' element={<ProDetails/>}></Route>
+</Routes>
+<Footer/>
+</BrowserRouter>
+
+</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,12 +5,27 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
-
+import store from './Redux/Store';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'remixicon/fonts/remixicon.css'
+import 'react-toastify/dist/ReactToastify.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+<Provider store={store}>
+<ToastContainer
+theme="dark"
+position="top-right"
+autoClose={3000}
+closeOnClick
+pauseOnHover={false}
+
+/>
+<App />
+</Provider>
+
   </React.StrictMode>
 );
 
